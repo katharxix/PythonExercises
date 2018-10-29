fc8915f5c566f290e53e60f7edaafa400c48c1e8
Karen Chaves <32829307+katharxix@users.noreply.github.com>
2018-10-29 14:08:26 -0500

    Subo taller String Formatting

---
+++ Exercisedec.py
@@ -0,0 +1,29 @@
+print ("Start the conversion!")
+entero = int(input("ingrese el numero: "))
+
+
+def convertobin(entero):
+    binario = ''
+    while entero // 2 != 0:
+        binario = str(entero % 2) + binario
+        entero = entero // 2
+    return str(entero) + binario
+
+
+def convert_oct(entero):
+    octal = ''
+    while entero // 8 !=0:
+        octal = str (entero % 8) + octal
+        entero = entero // 8
+    return str (entero) + octal
+
+
+def convert_hex (entero):
+    hexa= hex(entero)
+    hexa = hexa[2:]
+    return (hexa)
+
+i=1
+while i<=entero:
+    print(str(i) + " " + str(convert_oct(i)) + " " + str(convert_hex(i)).upper() + " " + str(convertobin(i)))
+    i=i+1
