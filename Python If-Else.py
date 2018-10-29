d7bf07b39ffe4049e890bf7e81ea280e87ca6e7c
Karen Chaves <32829307+katharxix@users.noreply.github.com>
2018-10-29 14:17:15 -0500

    Remove unnecessary whitespace

--- Python If-Else.py
+++ Python If-Else.py
@@ -1,16 +1,13 @@
 print ("Weird Numbers")
-print("*************************")
 num = int(input("Enter your number: "))
 
 def odd_num(num):
-
     if (num%2 !=0 and num<=100) or (6<=num<=20 and num<=100):
             answer='Weirdo'
     elif (num%2==0 and 2<=num<=5 and num<=100) or (num>20 and num<=100):
             answer='Not Weird'
     else:
         answer="¡Number out of Range!"
-
     return answer
 
 print(odd_num(int(num)))
